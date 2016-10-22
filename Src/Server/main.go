e2da96c3aaac7142a6678ba5e9941efeac21bd8d
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-10-22 13:58:22 +0200

    Basic receive func

---
+++ Src/Server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	Receive()
+
+	fmt.Printf("siema")
+}
---
+++ Src/Server/queueManager.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func Receive() {
+	conn, err := amqp.Dial("amqp://guest:guest@cerbercam.cloudapp.net:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	q, err := ch.QueueDeclare(
+		"picturesQueue", // name
+		false,           // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	go func() {
+		for d := range msgs {
+			fmt.Print("Received a message: %s", d.Body)
+		}
+	}()
+
+	defer ch.Close()
+	defer conn.Close()
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
