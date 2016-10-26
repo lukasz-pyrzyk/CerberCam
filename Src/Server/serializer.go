b7978f019f8a09cb1361d4ac00d42db8a505f8a3
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-10-26 17:50:07 +0200

    Updated logging

--- Src/Server/serializer.go
+++ Src/Server/serializer.go
@@ -4,6 +4,8 @@ import proto "github.com/golang/protobuf/proto"
 
 // Deserialize
 func Deserialize(body []byte) *Message {
+	log.Debugf("Deserializing message with %d bytes", len(body))
+
 	msg := &Message{}
 	err := proto.Unmarshal(body, msg)
 
