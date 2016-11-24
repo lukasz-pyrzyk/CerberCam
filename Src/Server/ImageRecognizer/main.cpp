402771c565135f112dba16421c239c0eac37f559
MrJaqbq <bentkowski.jakub@gmail.com>
2016-11-24 19:05:33 +0100

    ImgRecog data loading

--- Src/Server/ImageRecognizer/main.cpp
+++ Src/Server/ImageRecognizer/main.cpp
@@ -4,22 +4,61 @@
 
 #include <tensorflow/c_api.h>
 #include <cstdio>
+#include <fstream>
+#include <cstring>
+#include <cassert>
 
-int main()
+static const char* model_file = "Data/tensorflow_inception_graph.pb";
+static const char* label_file = "Data/imagenet_comp_graph_label_strings.txt";
+
+struct File
 {
-	TF_Graph* graph = TF_NewGraph();
+	FILE*			handle;
+	std::size_t		length;
+};
 
-	if(graph == nullptr)
-		return 1;
-
-	TF_Status* status = TF_NewStatus();
-	TF_Session* session = TF_NewSession(TF_NewSessionOptions(), status);
-
+void checkStatus(TF_Status* status)
+{
 	auto code = TF_GetCode(status);
 	if(code != TF_OK)
 	{
-		printf("%d : %s", code, TF_Message(status));
+		printf("Code %d : %s", code, TF_Message(status));
+		assert(false && "Stoped on Tensorflow error!");
 	}
+}
+
+File openFile(const char* path)
+{
+	File file;
+	file.handle = fopen(path, "r");
+	file.length = 0;
+
+	if(file.handle != nullptr)
+	{
+		file.length = (std::size_t) feof(file.handle);
+	}
+
+	return file;
+}
+
+int main()
+{
+	TF_Status* status = TF_NewStatus();
+	TF_Graph* graph = TF_NewGraph();
+
+	File model = openFile(model_file);
+
+	TF_Buffer* buffer = TF_NewBufferFromString(model.handle, model.length);
+
+	TF_ImportGraphDefOptions* importOptions = TF_NewImportGraphDefOptions();
+
+	TF_GraphImportGraphDef(graph, buffer, importOptions, status);
+
+	checkStatus(status);
+
+	TF_Session* session = TF_NewSession(TF_NewSessionOptions(), status);
+
+	checkStatus(status);
 
 	return 0;
 }
