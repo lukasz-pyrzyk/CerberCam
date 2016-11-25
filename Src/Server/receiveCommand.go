9fc6ffb00e1a225ac2984429a1fc71d25f573bc7
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-11-25 21:59:24 +0100

    Serializer as a struct

--- Src/Server/receiveCommand.go
+++ Src/Server/receiveCommand.go
@@ -3,9 +3,12 @@ package main
 // HandleReceiveCommand - handles getting data from queue and inserting to database
 func HandleReceiveCommand() {
 	log.Info("Checking for new data in queue...")
+
+	serializer := serializer{}
+
 	msgs := Receive(GlobalConfig.Queue.Topic)
 	for d := range msgs {
-		msg := Deserialize(d.Body)
+		msg := serializer.Deserialize(d.Body)
 		log.Infof("Received a message: %s", *msg.Email)
 
 		InsertToDatabase(msg)
--- Src/Server/serializer.go
+++ Src/Server/serializer.go
@@ -2,8 +2,11 @@ package main
 
 import proto "github.com/golang/protobuf/proto"
 
+type serializer struct {
+}
+
 // Deserialize
-func Deserialize(body []byte) *Message {
+func (s serializer) Deserialize(body []byte) *Message {
 	log.Debugf("Deserializing message with %d bytes", len(body))
 
 	msg := &Message{}
