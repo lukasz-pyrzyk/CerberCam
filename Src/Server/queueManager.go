5694be25330ef959489fd305e3936ef2ab4e6589
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-11-25 22:21:12 +0100

    Refactor of the queue manager

--- Src/Server/queueManager.go
+++ Src/Server/queueManager.go
@@ -5,10 +5,47 @@ import "github.com/streadway/amqp"
 type queueManager struct {
 }
 
-func closeQueue(queue amqp.Queue, channel *amqp.Channel, connection *amqp.Connection) {
-	log.Debugf("Closing queue '%s' and its connections...", queue.Name)
-	defer channel.Close()
-	defer connection.Close()
+// Receive data from queue
+func (manager queueManager) Receive(queueName string) <-chan amqp.Delivery {
+	q, ch, conn := openQueue(queueName)
+	defer closeQueue(q, ch, conn)
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+
+	failOnError(err, "Failed to register a consumer")
+
+	return msgs
+}
+
+// Send data to queue
+func (manager queueManager) Send(queueName string) {
+	q, ch, conn := openQueue(queueName)
+	defer closeQueue(q, ch, conn)
+
+	log.Info("Sending message...")
+
+	body := "ALERT"
+	err := ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+
+	failOnError(err, "Failed to publish a message")
+
+	log.Info("Message sent successfully!")
 }
 
 func openQueue(queueName string) (amqp.Queue, *amqp.Channel, *amqp.Connection) {
@@ -29,50 +66,14 @@ func openQueue(queueName string) (amqp.Queue, *amqp.Channel, *amqp.Connection) {
 		false,     // no-wait
 		nil,       // arguments
 	)
+
 	failOnError(err, "Failed to declare a queue")
 
 	return q, ch, conn
 }
 
-// Send data to queue
-func (manager queueManager) Send(queueName string) {
-	q, ch, conn := openQueue(queueName)
-
-	log.Info("Sending message...")
-
-	body := "ALERT"
-	err := ch.Publish(
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-	failOnError(err, "Failed to publish a message")
-
-	log.Info("Message sent successfully!")
-
-	defer closeQueue(q, ch, conn)
-}
-
-// Receive data from queue
-func (manager queueManager) Receive(queueName string) <-chan amqp.Delivery {
-	q, ch, conn := openQueue(queueName)
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	defer closeQueue(q, ch, conn)
-
-	return msgs
+func closeQueue(queue amqp.Queue, channel *amqp.Channel, connection *amqp.Connection) {
+	log.Debugf("Closing queue '%s' and its connections...", queue.Name)
+	defer channel.Close()
+	defer connection.Close()
 }
