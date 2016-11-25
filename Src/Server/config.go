510fd2ca379511f9b139ff81ba0a7f64fba58673
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-11-25 19:39:19 +0100

    Added structures for config

---
+++ Src/Server/config.go
@@ -0,0 +1,23 @@
+package main
+
+type config struct {
+	Tensorflow tensorflowConfig
+	Mongo      mongoConfig
+	Queue      queueConfig
+}
+
+type queueConfig struct {
+	Host  string
+	Topic string
+}
+
+type tensorflowConfig struct {
+	ModelDir string
+	Host     string
+}
+
+type mongoConfig struct {
+	Host          string
+	Database      string
+	MessagesTable string
+}
