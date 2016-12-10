c9dd8b009e7f19705575d59f79981e022a7580ff
MrJaqbq <bentkowski.jakub@gmail.com>
2016-12-10 16:47:35 +0100

    Added photon css and basic html

--- Src/ElectronClient/messages.js
+++ Src/ElectronClient/messages.js
@@ -5,8 +5,8 @@ module.exports = {
         // pass a proto file as a buffer/string or pass a parsed protobuf-schema object
         var proto = protobuf(fs.readFileSync('../Messages/message.proto'))
         var buf = proto.Message.encode({
-            Email: "lukasz.pyrzyk@gmail.com",
-            Photo: fs.readFileSync('/home/lukasz/Dev/iphone')
+            Email: "lukasz.pyrzyk@gmail.com"
+            //Photo: fs.readFileSync('/home/lukasz/Dev/iphone')
         });
         
         return buf;
