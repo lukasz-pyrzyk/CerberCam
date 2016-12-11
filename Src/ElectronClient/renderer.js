5e6eb5c46b344320c735614b27ae9ed8cc6f682d
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-11 09:23:25 +0100

    Added native dialog

--- Src/ElectronClient/renderer.js
+++ Src/ElectronClient/renderer.js
@@ -1,12 +1,33 @@
 const electron = require('electron')
+const {dialog} = require('electron').remote
+const fs = require('fs');
 
 const ipc = electron.ipcRenderer;
+var fileName
+
+document.getElementById('selectFile').addEventListener('click', _ => {
+    dialogOptions = {
+        filters: [
+            {
+                name: 'Images', extensions: ['jpg', 'png', 'gif']
+            }
+        ],
+        properties: [
+            'openFile'
+        ]
+    }
+    dialog.showOpenDialog(dialogOptions, function(data){
+        fileName = data[0]
+        console.log(fileName)
+    })
+
+})
 
 document.getElementById('send').addEventListener('click', _ => {
     var email = document.getElementById('email').value
     console.log('Clicked \'start\' button, sending data through the ipc')
 
-    var buffer = new Buffer('lorem ipsum') // TODO: Jakub, get bytes from user's photo
+    var buffer = fs.readFileSync(fileName)
     var msg = {
         Email: email,
         Photo: buffer
